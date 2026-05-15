8976d0e7dbd95c948ee90e1b074b95903ee3cc87
Jason Hughes <jasonhughes634@gmail.com>
2026-05-15 15:24:06 +0100

    Add: data generator function, it makes a csv of proc ess data in the static folder

---
+++ data_generator.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+from random import randint
+import numpy as np 
+
+num_processes = randint(30, 60)
+print(f"Number of processes created : {num_processes}")
+data_list = []
+# Creates a dataframe that contains Burst time, Process id, Arrival time . There should be a minimum of 30 processes 
+for process in range(0, num_processes ):
+    arrival_time = randint(0, 30)
+    burst_time = randint(0, 50)
+    # print(f"Process {process}: arrival time: {arrival_time},Burst time: {burst_time} ")
+    data_list.append([arrival_time, burst_time])
+
+data = pd.DataFrame(np.array(data_list), columns=["arrival_time", "burst_time"])
+
+
+
+# Saves the dataframe in a csv format in the directory 
+
+data = data.rename_axis("process_id")
+
+data.to_csv("./static/data")
